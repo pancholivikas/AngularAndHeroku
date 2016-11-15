25ee64fcc87dad6adae3ce687c3d66004bd65f41
Vikas Pancholi <panchv1@cat.com>
2016-11-15 11:48:46 +0530

    setting the module for routes

--- views/app/path-details/pathdetails.route.js
+++ views/app/path-details/pathdetails.route.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-    angular.module( 'app' )
+    angular.module( 'app.pathdetails' )
         .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
             $stateProvider
                 .state('path-details',
